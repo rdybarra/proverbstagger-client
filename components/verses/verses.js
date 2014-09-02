ab92cf1c8a1eb41b7f6cebcc8d560178772ac702
Ricky Ybarra <rdybarra@gmail.com>
2014-09-01 18:16:49 -0700

    WIP towards options.

--- components/verses/verses.js
+++ components/verses/verses.js
@@ -26,6 +26,17 @@
 
   } ]);
 
+  app.controller("SearchController", [ '$location', function($location) {
+
+    var self = this;
+    self.verses = [];
+
+    this.doSearch = function() {
+      $location.path('/keyword/' + this.term);
+    };
+
+  } ]);
+
   app.directive('keywordForm', function() {
     return {
       restrict: 'A',
@@ -37,26 +48,33 @@
         this.addAssociation = function(proverb) {
           var unique = true;
 
-          for (var i = proverb.keyword_associations.length - 1; i >= 0; i--) {
+          for (var i = 0; i < proverb.keyword_associations.length; i++) {
             if (this.keyword.value == proverb.keyword_associations[i].keyword.value) {
-              proverb.keyword_associations[i].count += 1;
               unique = false;
+              var foundAssociation = proverb.keyword_associations[i];
+
+              var data = {
+                'verse_id': foundAssociation.verse_id,
+                'keyword_id': foundAssociation.keyword_id,
+                'count': foundAssociation.count + 1
+              };
+
+              $http({ method: 'PATCH', url: 'http://localhost:3000/api/keyword_associations/' + foundAssociation.id, data: angular.toJson(data)}).success(function(association) {
+                foundAssociation.count = association.count;
+              });
             }
           };
 
           if (unique) {
-            this.association.keyword = this.keyword;
-            this.association.count = 1;
-            proverb.keyword_associations.push(this.association);
+            var data = {
+              'verse_id': proverb.id,
+              'keyword': this.keyword
+            };
+
+            $http.post('http://localhost:3000/api/keyword_associations', data).success(function(association) {
+              proverb.keyword_associations.push(association);
+            });
           }
-
-          var data = {
-            'keyword': this.keyword
-          };
-
-          $http.put('http://localhost:3000/api/verse/' + proverb.id, data).success(function(data) {
-            console.log('saved');
-          });
 
           this.keyword = {};
         };
@@ -64,5 +82,32 @@
       controllerAs: 'association'
     };
   });
+
+  app.directive('keywordOptions', function() {
+    return {
+      restrict: 'A',
+      templateUrl: 'components/verses/templates/keyword-options.html',
+      controller: function($http) {
+          this.canDelete = function(association) {
+            //if (association.ip_address == '127.0.0.1' && ''
+            
+            var created = new Date(association.created_at);
+            var now = new Date();
+            var offset = 10800000; // 15 minutes
+
+            if (now - created < offset) {
+              return true;
+            }
+
+            return false;
+          },
+          this.delete = function(association) {
+            console.log('delte');
+          }
+      },
+      controllerAs: 'options'
+    };
+  });
+
 
 })();
